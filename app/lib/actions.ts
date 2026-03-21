f9788851294f070062ddd86c08d1da24633af11e
Sergey Harchenko <sergdevguy@gmail.com>
2026-03-21 17:52:23 +0300

    add error pages

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,10 +27,17 @@ export async function createInvoice(formData: FormData) {
 	const amountInCents = amount * 100
 	const date = new Date().toISOString().split('T')[0]
 
-	await sql`
+	try {
+		await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `
+	} catch (error) {
+		console.error(error)
+		return {
+			message: 'Database Error: Failed to Create Invoice.'
+		}
+	}
 
 	revalidatePath('/dashboard/invoices')
 	redirect('/dashboard/invoices')
@@ -45,11 +52,18 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 	const amountInCents = amount * 100
 
-	await sql`
+	try {
+		await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `
+	} catch (error) {
+		console.error(error)
+		return {
+			message: 'Database Error: Failed to Update Invoice.'
+		}
+	}
 
 	revalidatePath('/dashboard/invoices')
 	redirect('/dashboard/invoices')
